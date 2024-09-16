ba959c7bef9b25f5d4f312b8f99f1ff059993f82
Matheus Valandro <msvalandro@proton.me>
2024-09-16 18:16:30 +0100

    feat: open alert dialog when clicking add item button

--- app/src/main/java/com/msvalandro/myshoppinglist/MainActivity.kt
+++ app/src/main/java/com/msvalandro/myshoppinglist/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.msvalandro.myshoppinglist.ui.theme.MyShoppingListTheme
@@ -17,7 +21,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyShoppingListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
+                Scaffold(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(WindowInsets.systemBars.asPaddingValues())) {
                     ShoppingList()
                 }
             }
--- app/src/main/java/com/msvalandro/myshoppinglist/ShoppingList.kt
+++ app/src/main/java/com/msvalandro/myshoppinglist/ShoppingList.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,16 +26,18 @@ data class ShoppingItem(
     var isEditing: Boolean
 )
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShoppingList() {
     var shoppingItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
+    var showDialog by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
         Button(
-            onClick = { /*TODO*/ },
+            onClick = { showDialog = true },
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text("Add Item")
@@ -44,6 +48,12 @@ fun ShoppingList() {
             items(shoppingItems) {
 
             }
+        }
+    }
+
+    if (showDialog) {
+        BasicAlertDialog(onDismissRequest = { showDialog = false }) {
+            Text("I'm an alert dialog!")
         }
     }
 }
