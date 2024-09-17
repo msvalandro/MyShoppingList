f5657eca7b38285f25b879783d3c8ba7844964b6
Matheus Valandro <msvalandro@proton.me>
2024-09-17 16:52:40 +0100

    feat: add shopping list item component

--- app/src/main/java/com/msvalandro/myshoppinglist/ShoppingList.kt
+++ app/src/main/java/com/msvalandro/myshoppinglist/ShoppingList.kt
@@ -1,5 +1,7 @@
 package com.msvalandro.myshoppinglist
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
@@ -19,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(
@@ -49,7 +53,7 @@ fun ShoppingList() {
             .fillMaxSize()
             .padding(16.dp)) {
             items(shoppingItems) {
-
+                ShoppingListItem(it, {}, {})
             }
         }
     }
@@ -108,5 +112,24 @@ fun ShoppingList() {
                 }
             }
         )
+    }
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .border(
+                border = BorderStroke(2.dp, Color(0XFF018786)),
+                shape = RoundedCornerShape(20)
+            )
+    ) {
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
     }
 }
